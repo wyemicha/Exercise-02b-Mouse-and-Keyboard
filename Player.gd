496cca3a18a3265fe2f75853c9370a738cd81099
Wyeth <wyemicha@iu.edu>
2021-02-09 22:54:26 -0500

    Godot workshop

--- Player.gd
+++ Player.gd
@@ -12,7 +12,10 @@ func _ready():
 	max_range = get_viewport().size
 
 func _physics_process(delta):
-
+	var r = get_viewport().get_mouse_position().x
+	midpoint = position.x
+	if abs(r - midpoint) > margin:
+		rotation_degrees += (r - midpoint)*rot_amount
 	velocity += get_input()*acc_amount
 	position += velocity
 	position.x = clamp(position.x,min_range.x,max_range.x)
@@ -20,4 +23,12 @@ func _physics_process(delta):
 
 func get_input():
 	var input_dir = Vector2(0,0)
+	if Input.is_action_pressed("up"):
+		input_dir.y -= 1
+	if Input.is_action_pressed("down"):
+		input_dir.y += 1
+	if Input.is_action_pressed("left"):
+		input_dir.x -= 1
+	if Input.is_action_pressed("right"):
+		input_dir.x += 1
 	return input_dir.rotated(rotation)
